f6943a23c86abf3256f0bc5ed3b9c57e8a698b79
codeeterna <kjnkrb0930@gmail.com>
2023-12-05 16:19:44 +0900

    hj 1

---
+++ lazybee/src/main/java/com/lazybee/LazybeeApplication.java
@@ -0,0 +1,13 @@
+package com.lazybee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LazybeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LazybeeApplication.class, args);
+	}
+
+}
---
+++ lazybee/src/test/java/com/lazybee/LazybeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lazybee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LazybeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
